Fixed: cannot renormalize period 5 and above Fixed: override the remove method from list class Added: Customize precision Added: Dynamically update the resolution of the graph when the plot is zoomed

PlotCurve.py
@@ -57,12 +57,12 @@ class GraphObjectBase():
         '''
         return self._visible & self._visibleMask 
 
-    # remove the graph from the screen
-    def remove(self):
-        raise NotImplementedError("GraphObjectBase.remove has to be implemented")
+    # clear the graph from the screen
+    def clear(self):
+        raise NotImplementedError("GraphObjectBase.clear has to be implemented")
 
 # Sync a group of GraphObjectBase items
-# sync methods: visible, remove
+# sync methods: visible, clear
 class GroupG(GraphObjectBase,list):
     def __init__(self,*args,visible=True):
         list.__init__(self,*args)
@@ -73,10 +73,11 @@ class GroupG(GraphObjectBase,list):
         for member in self:
             member.setVisibleMask(visible)
     
-    # remove all artist in the list from the canvas    
-    def remove(self):
+    # clear all artist in the list from the canvas    
+    def clear(self):
         for member in self:
-            member.remove()
+            member.clear()
+        del self[:]
 
 # Graphical object for artist
 # line=matplotlib.lines.Line2D object
@@ -101,8 +102,11 @@ class GraphObject(GraphObjectBase,QtCore.QObject):
         raise NotImplementedError("GraphObjectBase._initilize has to be implemented")
 
     # artist: matplotlib artist
-    def _updatePlot(self,curve):
+    def _updatePlot(self, artist):
         raise NotImplementedError("GraphObjectBase._updatePlot has to be implemented")
+
+    def _clearPlot(self, artist):
+        pass
 
     @QtCore.pyqtSlot()
     def update(self):
@@ -139,22 +143,43 @@ class GraphObject(GraphObjectBase,QtCore.QObject):
         self._artist=curve
     artist=property(getArtist, setArtist)
 
-    # remove the artist from the canvas    
-    def remove(self):
+    # clear the artist from the canvas    
+    def clear(self):
         if self._artist is not None:
+            self._clearPlot(self._artist)
             self._artist.remove()
             self._artist=None
             self._canvas.update()
 
-# Plot of a function
+def _generateSample(axis):
+    l,r = axis.get_xlim()
+    sample = np.append(np.arange(-1.0, l, 0.001), np.arange(l, r, (r-l)/1000.0))
+    sample = np.append(sample, np.arange(r, 1.001, 0.001))
+    return sample
+
 class FunctionG(GraphObject):
+    '''
+    Plot a function
+    '''
 
     # Function of one variable
     _func=None
+    _axis=None
+    _xEventId=None
+    _yEventId=None
     
-    # canvas: canvas to show the plot
-    # func: function of one variable
     def __init__(self, canvas, func, axis=None, visible=True, **kwargs):
+        '''
+        Plot a function
+        :param canvas: the canvas showing the plot
+        :type canvas:
+        :param func: function to be plot
+        :type func: function of one variable
+        :param axis: axis to be plot. 
+        :type axis:
+        :param visible: set visible 
+        :type visible:
+        '''
         self._func=func
         self._kwargs=kwargs
         if axis == None:
@@ -163,12 +188,20 @@ class FunctionG(GraphObject):
             self._axis=axis
 
         # set sample points
-        self._sample = np.arange(-1.0, 1.001, 0.0001)
+        self._sample = _generateSample(self._axis)
 
         super().__init__(canvas, visible=visible)
-        
+    
+    
     def _initilizePlot(self):
-        return self.__plot(self._axis)
+        artist = self.__plot(self._axis)
+
+        def on_xlims_change(axis):
+            self._sample = _generateSample(axis)
+            self.update()
+        self._xEventId=self._axis.callbacks.connect('xlim_changed', on_xlims_change)
+
+        return artist
 
     def __plot(self, axis):
         curve, = axis.plot(self._sample, self._func(self._sample), **self._kwargs)
@@ -181,7 +214,14 @@ class FunctionG(GraphObject):
             return None
 
     def _updatePlot(self,curve):
+        curve.set_xdata(self._sample)
         curve.set_ydata(self._func(self._sample))
+
+    def _clearPlot(self, artist):
+        if self._xEventId != None:
+            self._axis.callbacks.disconnect(self._xEventId)
+            self._xEventId=None
+        GraphObject._clearPlot(self, artist)
 
     # function
     def getFunction(self):
@@ -207,7 +247,7 @@ class ContourG(GraphObjectBase,QtCore.QObject):
         GraphObjectBase.__init__(self,visible=visible)
 
         # set sample points
-        x = np.arange(-1.0, 1.0, 0.0001)
+        x = _generateSample(self._canvas.axes)
         y = np.arange(-1.0, 1.1, 2)
         self.sampleX,self.sampleY = np.meshgrid(x,y)
         
@@ -231,14 +271,14 @@ class ContourG(GraphObjectBase,QtCore.QObject):
 
     def _updatePlot(self):
         for item in self._contour.collections:
-            item.remove()
+            item.clear()
         self._contour = self._canvas.axes.contourf(self.sampleX, self.sampleY, self._func(self.sampleX,self.sampleY),**self._kwargs)
     
     def _removePlot(self):
         for item in self._contour.collections:
-            item.remove()
+            item.clear()
         self._contour=None
-        self._cbaxes.remove()
+        self._cbaxes.clear()
         self._cbaxes=None
         self._cbar=None
     
@@ -268,7 +308,7 @@ class ContourG(GraphObjectBase,QtCore.QObject):
         self._func=func
         self.update()
         
-    def remove(self):
+    def clear(self):
         if self._contour is not None:
             self._removePlot()
             self._canvas.update()

Unimodal.py
@@ -7,8 +7,13 @@ import time
 #import timeit
 import Setting
 import traceback
+from enum import Enum
 
-#import error
+class Renormalizable(Enum):
+    under=False
+    renormalizable=True
+    above=False
+    pass
 
 class Affine:
     def __init__(self, x1, y1, x2, y2):
@@ -24,7 +29,7 @@ def fixedPoint(func):
     pass
 # 
 class Unimodal:
-    """A simple example class"""
+    """A unimodal class"""
     __TOL_EQUAL = np.float64(1e-02)
     
     _map = None
@@ -78,17 +83,17 @@ class Unimodal:
         # Find beta fixed point
         #self.p_b=optimize.fixed_point(self._map, (self.p_c+self.p_A)/np.float64(2))
         try:
-            self.p_b=optimize.brenth(lambda x: self._map(x)-x, self.p_c, self.p_A)
+            self.p_b=optimize.brenth(lambda x: self._map(x)-x, self.p_c, self.p_A, xtol=Setting.precisionPeriodicA, rtol=Setting.precisionPeriodicR)
         except BaseException as e:
-            raise RuntimeError("Unimodal.__init__: Unable to find beta fixed point")
+            raise RuntimeError("Unimodal.__init__: Unable to find beta fixed point\n"+str(e))
         try:
-            self.p_B=optimize.brenth(self.__map_solve, self.p_a, self.p_c, args=(self.p_b,))
+            self.p_B=optimize.brenth(self.__map_solve, self.p_a, self.p_c, args=(self.p_b,), xtol=Setting.precisionPeriodicA, rtol=Setting.precisionPeriodicR)
         except BaseException as e:
-            raise RuntimeError("Unimodal.__init__: Unable to find beta_bar point")
+            raise RuntimeError("Unimodal.__init__: Unable to find beta_bar point"+str(e))
         try:
-            self.p_B2=optimize.brenth(self.__map_solve, self.p_b, self.p_A, args=(self.p_B,))
+            self.p_B2=optimize.brenth(self.__map_solve, self.p_b, self.p_A, args=(self.p_B,), xtol=Setting.precisionPeriodicA, rtol=Setting.precisionPeriodicR)
         except BaseException as e:
-            raise RuntimeError("Unimodal.__init__: Unable to find beta_2bar point")
+            raise RuntimeError("Unimodal.__init__: Unable to find beta_2bar point"+str(e))
         
     def __call__(self, x):
         return self._map(x)
@@ -118,8 +123,8 @@ class Unimodal:
             elif period % 2 == 1:
                 self._renormalizable[period]=self.renomalizableOdd(period)
             else:
-                # not implimented
-                pass
+                # Not implimented
+                self._renormalizable[period]=False
         return self._renormalizable[period]
 
     def renomalize(self, period=2):
@@ -141,15 +146,24 @@ class Unimodal:
     def renomalizable2(self):
         '''
         Check if the unimodal map is period-doubling renormalizable
-        :return: bool
+        :return: enum Renormalizable
         '''
         # The condition f(v) < c ensures that there exists a fixed point with negative multiplier
-        result = self.p_b < self.p_v and self.p_v < self.p_B2 and self._map(self.p_v) < self.p_c
-        if result == True:
-            self.p_a1[2]=[self.p_b,self.p_b]
-            self.p_A1[2]=[self.p_B2,self.p_B]
+        if not self.p_b < self.p_v:
+            #return Renormalizable.under
+            return False
+        if not self._map(self.p_v) < self.p_c:
+            #return Renormalizable.under
+            return False
+        if not self.p_v < self.p_B2:
+            #return Renormalizable.above
+            return False
+            
+        self.p_a1[2]=[self.p_b,self.p_b]
+        self.p_A1[2]=[self.p_B2,self.p_B]
         
-        return result
+        #return Renormalizable.renormalizable
+        return True
 
     def renomalize2(self):
         # check if the function is renormalizable
@@ -178,8 +192,11 @@ class Unimodal:
             maximalPoints=self.preimage(maximalPoints)
 
         # Search for the periodic points
+        maximalPoints=[x for x in maximalPoints if x<self.p_v]
+        otherLocalExtremals=[x for x in otherLocalExtremals if x<self.p_v]
         maxPoint=max(maximalPoints)
         minPoint=max(otherLocalExtremals)
+        #print("max:", maxPoint, ", min:", minPoint)
 
         def __iteration(x):
             y=x
@@ -188,12 +205,13 @@ class Unimodal:
             return y-x
         
         # need f^p(c)>c => __iteration(minPoint) > 0
-        if maxPoint > self.p_v:
+        #if not maxPoint < self.p_v:
+        if not __iteration(maxPoint) > 0:
             return False
-        if __iteration(minPoint) > 0:
+        if not __iteration(minPoint) < 0:
             return False
         
-        periodicPoint=optimize.brenth(__iteration, max(otherLocalExtremals), max(maximalPoints))
+        periodicPoint=optimize.brenth(__iteration, minPoint, maxPoint, xtol=Setting.precisionPeriodicA, rtol=Setting.precisionPeriodicR)
         #print("periodic point: ",periodicPoint, "    critical value:",self.p_v)
         periodicOrbit=self.orbit(periodicPoint, period)
         periodicReflex=self.reflexOrbit(periodicOrbit)
@@ -248,17 +266,17 @@ class Unimodal:
         point=p_inOrbit[0]        
         # Build p-1
         if p_inOrbit[period-1] < self.p_c:
-            point=optimize.brenth(lambda x: self._map(x)-point,self.p_c,self.p_A)
+            point=optimize.brenth(lambda x: self._map(x)-point,self.p_c,self.p_A, xtol=Setting.precisionPeriodicA, rtol=Setting.precisionPeriodicR)
         else:
-            point=optimize.brenth(lambda x: self._map(x)-point,self.p_a,self.p_c)
+            point=optimize.brenth(lambda x: self._map(x)-point,self.p_a,self.p_c, xtol=Setting.precisionPeriodicA, rtol=Setting.precisionPeriodicR)
         p_outOrbit[period-1]=point
         
         t=period-2
         while t >= 0:
             if p_inOrbit[t] < self.p_c:
-                point=optimize.brenth(lambda x: self._map(x)-point,self.p_a,self.p_c)
+                point=optimize.brenth(lambda x: self._map(x)-point,self.p_a,self.p_c, xtol=Setting.precisionPeriodicA, rtol=Setting.precisionPeriodicR)
             else:
-                point=optimize.brenth(lambda x: self._map(x)-point,self.p_c,self.p_A)
+                point=optimize.brenth(lambda x: self._map(x)-point,self.p_c,self.p_A, xtol=Setting.precisionPeriodicA, rtol=Setting.precisionPeriodicR)
             p_outOrbit[t]=point
             t=t-1
         return p_outOrbit
@@ -275,8 +293,8 @@ class Unimodal:
             
         for pt in points:
             if pt < self.p_v:
-                result.append(optimize.brenth(lambda x: self._map(x)-pt,self.p_a,self.p_c))
-                result.append(optimize.brenth(lambda x: self._map(x)-pt,self.p_c,self.p_A))
+                result.append(optimize.brenth(lambda x: self._map(x)-pt,self.p_a,self.p_c, xtol=Setting.precisionPeriodicA, rtol=Setting.precisionPeriodicR))
+                result.append(optimize.brenth(lambda x: self._map(x)-pt,self.p_c,self.p_A, xtol=Setting.precisionPeriodicA, rtol=Setting.precisionPeriodicR))
 
         return result
     